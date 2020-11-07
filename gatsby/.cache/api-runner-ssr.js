bc81389c32e4333c47a619a2659a9145f04cd590
Derek Glassick <derek.glassick@gmail.com>
2020-11-06 17:11:19 -0800

    working on css and gatsby pages.

--- gatsby/.cache/api-runner-ssr.js
+++ gatsby/.cache/api-runner-ssr.js
@@ -1,4 +1,7 @@
-var plugins = []
+var plugins = [{
+      plugin: require('C:/Users/Derek/coding-bootcamp/Udemy Course/Wes Bos/pizza-site-gatsby-react/gatsby/gatsby-ssr'),
+      options: {"plugins":[]},
+    }]
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
 //   {
