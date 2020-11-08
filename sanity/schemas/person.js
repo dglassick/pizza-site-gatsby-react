7a0bf63cdaaa7c7814571aa0ec19d262fd160a76
Derek Glassick <derek.glassick@gmail.com>
2020-11-07 17:27:20 -0800

    added the ability to make persons who work at the shop. also added price component into sanity studio

---
+++ sanity/schemas/person.js
@@ -0,0 +1,40 @@
+import { MdPerson as icon } from 'react-icons/md';
+
+export default {
+  // computer name
+  name: 'peron',
+  // visible title
+  title: 'Slicemasters',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'description',
+      title: 'description',
+      type: 'text',
+      description: 'Tell us a bit about this person.',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+};
