6a1c355b3e38561e55f69b4f3c04dd235b53ebef
Derek Glassick <derek.glassick@gmail.com>
2020-12-02 08:22:27 -0800

    created honey pot, working on main page and creating a backend where someone can modify store locations if needed.

--- gatsby/src/pages/index.js
+++ gatsby/src/pages/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import LoadingGrid from '../components/LoadingGrid';
 import Nav from '../components/Nav';
+import { HomePageGrid } from '../styles/Grids';
+import useLatestData from '../utils/useLatestData';
+
+function CurrentlySlicing({ slicemasters }) {
+  return (
+    <div>
+      {!slicemasters && <LoadingGrid count={4} />}
+      {slicemasters && !slicemasters?.length && (
+        <p>No one is working right now!</p>
+      )}
+    </div>
+  );
+}
+
+function HotSlices({ hotslices }) {
+  return (
+    <div>
+      {!hotslices && <LoadingGrid count={4} />}
+      {hotslices && !hotslices?.length && <p>Nothin' in the case.</p>}
+    </div>
+  );
+}
 
 export default function HomePage() {
+  const { hotslices, slicemasters } = useLatestData();
   return (
-    <>
-      <p>Hey! I'm the home page</p>
-    </>
+    <div className="center">
+      <h1>The Best Pizza Downtown</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+
+      <HomePageGrid>
+        <CurrentlySlicing slicemasters={slicemasters} />
+
+        <HotSlices hotslices={hotslices} />
+      </HomePageGrid>
+    </div>
   );
 }
