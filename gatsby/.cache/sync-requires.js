fc2e38760e5a66e68e4f681e61bb6d982327cc30
Derek Glassick <derek.glassick@gmail.com>
2020-12-03 10:22:41 -0800

    forgot to rerun npm run build

--- gatsby/.cache/sync-requires.js
+++ gatsby/.cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": (preferDefault(require("C:\\Users\\Derek\\coding-bootcamp\\Udemy Course\\Wes Bos\\pizza-site-gatsby-react\\gatsby\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": (preferDefault(require("C:\\Users\\Derek\\coding-bootcamp\\Udemy Course\\Wes Bos\\pizza-site-gatsby-react\\gatsby\\src\\pages\\404.js"))),
   "component---src-pages-beers-js": (preferDefault(require("C:\\Users\\Derek\\coding-bootcamp\\Udemy Course\\Wes Bos\\pizza-site-gatsby-react\\gatsby\\src\\pages\\beers.js"))),
   "component---src-pages-index-js": (preferDefault(require("C:\\Users\\Derek\\coding-bootcamp\\Udemy Course\\Wes Bos\\pizza-site-gatsby-react\\gatsby\\src\\pages\\index.js"))),
